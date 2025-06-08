5f9ccae1c94579aab86d2f2b3c243ade4bb63ebe
liuyuhui <liuyuhui@99digtech.com>
2025-06-08 21:50:53 +0800

    fix:兼容rander

--- app.py
+++ app.py
@@ -78,4 +78,7 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
